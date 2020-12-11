ca1ebeb8abe593f7459faccd905ed73f7cb42750
KarlisKV <kvelins@gmail.com>
2020-12-11 18:44:05 +0200

    Added javaDoc for Level2 and EPFLLevel and cleaned up some code

--- java/ch/epfl/cs107/play/game/superpacman/area/levels/Level2.java
+++ java/ch/epfl/cs107/play/game/superpacman/area/levels/Level2.java
@@ -32,53 +32,76 @@ public class Level2 extends SuperPacmanArea {
         registerActor(new Door(Level3.LEVEL_PATHNAME, Level3.PLAYER_SPAWN_POSITION, Logic.TRUE,
                                this, Orientation.DOWN, new DiscreteCoordinates(14, 0), new DiscreteCoordinates(15, 0)));
 
-        // Keys
-        // TODO: add descriptif name to keys (ex: key1 -> keyTopLeft, ...) then update the keys below for the gates
+        //Create HashMap with the list of keys
         HashMap<String, Key> keyList = new HashMap<>();
-        keyList.put("key1", createKey(new DiscreteCoordinates(3, 16)));
-        keyList.put("key2", createKey(new DiscreteCoordinates(26, 16)));
-        keyList.put("key3", createKey(new DiscreteCoordinates(2, 8)));
-        keyList.put("key4", createKey(new DiscreteCoordinates(27, 8)));
+
+        keyList.put("keyTopLeft", createKey(new DiscreteCoordinates(3, 16)));
+        keyList.put("keyTopRight", createKey(new DiscreteCoordinates(26, 16)));
+        keyList.put("keyBottomLeft", createKey(new DiscreteCoordinates(2, 8)));
+        keyList.put("keyBottomRight", createKey(new DiscreteCoordinates(27, 8)));
         // Register all keys from map
         for (Key key : keyList.values()) {
             registerActor(key);
         }
 
         // Create and register gates
-        createGate(Orientation.RIGHT, new DiscreteCoordinates(8, 14), keyList.get("key1"));
-        createGate(Orientation.DOWN, new DiscreteCoordinates(5, 12), keyList.get("key1"));
-        createGate(Orientation.RIGHT, new DiscreteCoordinates(8, 10), keyList.get("key1"));
-        createGate(Orientation.RIGHT, new DiscreteCoordinates(8, 8), keyList.get("key1"));
+        createGate(Orientation.RIGHT, new DiscreteCoordinates(8, 14), keyList.get("keyTopLeft"));
+        createGate(Orientation.DOWN, new DiscreteCoordinates(5, 12), keyList.get("keyTopLeft"));
+        createGate(Orientation.RIGHT, new DiscreteCoordinates(8, 10), keyList.get("keyTopLeft"));
+        createGate(Orientation.RIGHT, new DiscreteCoordinates(8, 8), keyList.get("keyTopLeft"));
 
-        createGate(Orientation.RIGHT, new DiscreteCoordinates(21, 14), keyList.get("key2"));
-        createGate(Orientation.DOWN, new DiscreteCoordinates(24, 12), keyList.get("key2"));
-        createGate(Orientation.RIGHT, new DiscreteCoordinates(21, 10), keyList.get("key2"));
-        createGate(Orientation.RIGHT, new DiscreteCoordinates(21, 8), keyList.get("key2"));
+        createGate(Orientation.RIGHT, new DiscreteCoordinates(21, 14), keyList.get("keyTopRight"));
+        createGate(Orientation.DOWN, new DiscreteCoordinates(24, 12), keyList.get("keyTopRight"));
+        createGate(Orientation.RIGHT, new DiscreteCoordinates(21, 10), keyList.get("keyTopRight"));
+        createGate(Orientation.RIGHT, new DiscreteCoordinates(21, 8), keyList.get("keyTopRight"));
 
-        createRightOrientatedGate(new DiscreteCoordinates(10, 2), keyList.get("key3"), keyList.get("key4"));
-        createRightOrientatedGate(new DiscreteCoordinates(19, 2), keyList.get("key3"), keyList.get("key4"));
-        createRightOrientatedGate(new DiscreteCoordinates(12, 8), keyList.get("key3"), keyList.get("key4"));
-        createRightOrientatedGate(new DiscreteCoordinates(17, 8), keyList.get("key3"), keyList.get("key4"));
+        createRightOrientatedGate(new DiscreteCoordinates(10, 2), keyList.get("keyBottomLeft"),
+                keyList.get("keyBottomRight"));
+        createRightOrientatedGate(new DiscreteCoordinates(19, 2), keyList.get("keyBottomLeft"),
+                keyList.get("keyBottomRight"));
+        createRightOrientatedGate(new DiscreteCoordinates(12, 8), keyList.get("keyBottomLeft"),
+                keyList.get("keyBottomRight"));
+        createRightOrientatedGate(new DiscreteCoordinates(17, 8), keyList.get("keyBottomLeft"),
+                keyList.get("keyBottomRight"));
 
         createRightOrientatedGate(new DiscreteCoordinates(14, 3));
         createRightOrientatedGate(new DiscreteCoordinates(15, 3));
 
     }
-
+    /**
+     * Method for key creation
+     * @param position (DiscreteCoordinates) position of the key
+     * @return created key
+     */
     private Key createKey(DiscreteCoordinates position) {
         return new Key(this, position);
     }
 
+    /**
+     * Method for creating gate with one key
+     * @param orientation (Orientation) of the gate
+     * @param position (DiscreteCoordinates) coordinates of the gate
+     * @param key (Key) that is linked to the corresponding gate
+     */
     private void createGate(Orientation orientation, DiscreteCoordinates position, Key key) {
         registerActor(new Gate(this, orientation, position, Logic.FALSE, key));
 
     }
+    /**
+     * Method for creating gate with two keys
+     * @param position (DiscreteCoordinates) coordinates of the gate
+     * @param key1 (Key) that is linked to the corresponding gate
+     * @param key2 (Key) that is linked to the corresponding gate
+     */
 
     private void createRightOrientatedGate(DiscreteCoordinates position, Key key1, Key key2) {
         registerActor(new Gate(this, Orientation.RIGHT, position, Logic.FALSE, key1, key2));
 
     }
-
+    /**
+     * Method for creating gate whose signal depends on whether all pellets are picked up
+     * @param position (DiscreteCoordinates) coordinates of the gate
+     */
     private void createRightOrientatedGate(DiscreteCoordinates position) {
         registerActor(new Gate(this, Orientation.RIGHT, position, Logic.FALSE));
     }
--- java/ch/epfl/cs107/play/game/superpacman/area/levels/LevelEPFL.java
+++ java/ch/epfl/cs107/play/game/superpacman/area/levels/LevelEPFL.java
@@ -15,6 +15,8 @@ import ch.epfl.cs107.play.game.superpacman.area.SuperPacmanArea;
 import ch.epfl.cs107.play.math.DiscreteCoordinates;
 import ch.epfl.cs107.play.signal.logic.Logic;
 
+import java.util.HashMap;
+
 public class LevelEPFL extends SuperPacmanArea {
     public static final String LEVEL_PATHNAME = "superpacman/levelEPFL";
     public static final DiscreteCoordinates PLAYER_SPAWN_POSITION = new DiscreteCoordinates(15, 3);
@@ -27,28 +29,65 @@ public class LevelEPFL extends SuperPacmanArea {
 
     @Override
     protected void createArea() {
-        // Door
+
+        //Registers the door for the level
         registerActor(new Door(Level2.LEVEL_PATHNAME, Level2.PLAYER_SPAWN_POSITION, Logic.TRUE,
          this, Orientation.DOWN, new DiscreteCoordinates(14, 10), new DiscreteCoordinates(15, 10)));
-        // Gates
 
-        Key key1 = new Key(this, new DiscreteCoordinates(1, 1));
-        Key key2 = new Key(this, new DiscreteCoordinates(28, 1));
-        Key key3 = new Key(this, new DiscreteCoordinates(1, 9));
-        Key key4 = new Key(this, new DiscreteCoordinates(28, 9));
-        // Gates
-        registerActor(key1);
-        registerActor(key2);
-        registerActor(key3);
-        registerActor(key4);
+        //Create HashMap with the list of keys
+        HashMap<String, Key> keyList = new HashMap<>();
 
-        registerActor(new Gate(this, Orientation.LEFT, new DiscreteCoordinates(14, 9), Logic.FALSE, key1, key2));
-        registerActor(new Gate(this, Orientation.LEFT, new DiscreteCoordinates(15, 9), Logic.FALSE, key1, key2));
-        registerActor(new Gate(this, Orientation.LEFT, new DiscreteCoordinates(1, 2), Logic.FALSE, key4));
-        registerActor(new Gate(this, Orientation.DOWN, new DiscreteCoordinates(2, 1), Logic.FALSE, key4));
-        registerActor(new Gate(this, Orientation.DOWN, new DiscreteCoordinates(27, 1), Logic.FALSE, key3));
-        registerActor(new Gate(this, Orientation.LEFT, new DiscreteCoordinates(28, 2), Logic.FALSE, key3));
-        // Gates
+        keyList.put("keyTopLeft", createKey(new DiscreteCoordinates(1, 1)));
+        keyList.put("keyTopRight", createKey(new DiscreteCoordinates(28, 9)));
+        keyList.put("keyBottomLeft", createKey(new DiscreteCoordinates(1, 9)));
+        keyList.put("keyBottomRight", createKey(new DiscreteCoordinates(28, 1)));
+
+        // Register all keys from map
+        for (Key key : keyList.values()) {
+            registerActor(key);
+        }
+
+        //Gates for passage to next level
+        createRightOrientatedGate(new DiscreteCoordinates(14, 9), keyList.get("keyTopLeft"),
+                keyList.get("keyBottomRight"));
+        createRightOrientatedGate(new DiscreteCoordinates(15, 9), keyList.get("keyBottomLeft"),
+                keyList.get("keyBottomRight"));
+
+        //Gates for unlocking keys
+        createGate(Orientation.LEFT, new DiscreteCoordinates(1, 2), keyList.get("keyTopRight"));
+        createGate(Orientation.DOWN, new DiscreteCoordinates(2, 1), keyList.get("keyTopRight"));
+        createGate(Orientation.DOWN, new DiscreteCoordinates(27, 1), keyList.get("keyTopLeft"));
+        createGate(Orientation.LEFT, new DiscreteCoordinates(28, 2), keyList.get("keyTopLeft"));
+
+    }
+
+    /**
+     * Method for key creation
+     * @param position (DiscreteCoordinates) position of the key
+     * @return created key
+     */
+    private Key createKey(DiscreteCoordinates position) {
+        return new Key(this, position);
+    }
+
+    /**
+     * Method for creating gate with one key
+     * @param orientation (Orientation) of the gate
+     * @param position (DiscreteCoordinates) coordinates of the gate
+     * @param key (Key) that is linked to the corresponding gate
+     */
+    private void createGate(Orientation orientation, DiscreteCoordinates position, Key key) {
+        registerActor(new Gate(this, orientation, position, Logic.FALSE, key));
+    }
+
+    /**
+     * Method for creating gate with two keys
+     * @param position (DiscreteCoordinates) coordinates of the gate
+     * @param key1 (Key) that is linked to the corresponding gate
+     * @param key2 (Key) that is linked to the corresponding gate
+     */
+    private void createRightOrientatedGate(DiscreteCoordinates position, Key key1, Key key2) {
+        registerActor(new Gate(this, Orientation.RIGHT, position, Logic.FALSE, key1, key2));
 
     }
 
